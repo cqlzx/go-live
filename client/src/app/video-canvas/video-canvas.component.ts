512d912cfb2be004624a403e343e78644aa16fbd
cqlzx <bupt11lzx@163.com>
2017-11-24 17:33:25 -0800

    finish video canvas

--- client/src/app/video-canvas/video-canvas.component.ts
+++ client/src/app/video-canvas/video-canvas.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoCanvasComponent implements OnInit {
 
+  videoSrc : any = '';
+
   constructor() { }
 
   ngOnInit() {
+    // navigator.getUserMedia = navigator.getUserMedia;
+      // || navigator.webkitGetUserMedia
+      // || navigator.mozGetUserMedia
+      // || navigator.msgGetUserMedia;
+
+    if (navigator.getUserMedia) {
+      const constraints = {
+        video: true
+      };
+      navigator.getUserMedia(constraints, this.loadCam, this.loadFail);
+    }
   }
 
+  loadCam(stream) {
+    const url = window.URL.createObjectURL(stream);
+    console.log(url);
+    this.videoSrc = url;
+    console.log('Load camera successfully!');
+  }
+
+  loadFail() {
+    console.log('Load camera failed!');
+  }
 }
